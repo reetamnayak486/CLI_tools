f500dbfdb0168e18e82908a36e65142d44ba56da
reetam486itb <reetam486itb@gmail.com>
2024-07-23 20:56:37 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const yargs= require("yargs")
+const {argv}= yargs(process.argv);
+console.log({argv});
+const print_five_moves= async(pokemon)=>{
+    let response= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    let pokemon_object= await response.json();
+    const moves= pokemon_object.moves.map(({move})=> move.name);
+    // console.log(moves.slice(0,5));
+}
+
+print_five_moves(argv.pokemon);
+// console.log(argv.pokemon);
+console.log(process.argv);
