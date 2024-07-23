ea169445528079a3b814b2105580f8376cfc269e
reetam486itb <reetam486itb@gmail.com>
2024-07-23 21:39:29 +0530

    added the prompt feature

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
 
-const yargs= require("yargs")
-const {argv}= yargs(process.argv);
-console.log({argv});
+// const yargs= require("yargs")
+// const {argv}= yargs(process.argv);
+
+const inquirer=require("inquirer");
+
+// console.log({argv});
 const print_five_moves= async(pokemon)=>{
     let response= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     let pokemon_object= await response.json();
     const moves= pokemon_object.moves.map(({move})=> move.name);
-    // console.log(moves.slice(0,5));
+    console.log(moves.slice(0,5));
 }
 
-print_five_moves(argv.pokemon);
-// console.log(argv.pokemon);
-console.log(process.argv);
+// print_five_moves(argv.pokemon);
+// // console.log(argv.pokemon);
+// console.log(process.argv);
+
+const prompt= inquirer.createPromptModule();
+prompt([{
+        type: "input",
+        name: "pokemon",
+        message: "enter the name of a pokemon",
+    },
+]).then((answers)=>{
+    const pokemon=answers.pokemon;
+    // console.log(answers);
+    print_five_moves(pokemon);
+});
